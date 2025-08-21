943f9358656bc210b2553cece3b09b31881fe137
sahelip <s.paul@aexonic.com>
2025-08-21 17:21:33 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import { OpenAI } from "openai";
+import OpenAI from "openai";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-async function run() {
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: "Hello, how are you?" }],
-    })
-    console.log(response.choices[0].message.content);
-}
+const completion = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  messages: [{ role: "user", content: "Hello, how are you?" }],
+});
 
-run();
+console.log(completion.choices[0].message.content);
